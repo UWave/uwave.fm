e31310ea7bcfe8838c8a541e63521d5f6867ade9
Finn <thefinn93@thefinn93.com>
2014-05-09 15:45:10 -0700

    Playing with the new button size

--- head.php
+++ head.php
@@ -21,8 +21,8 @@
                                 <li><a href="https://twitter.com/UWaveRadio" target = "_blank"><i class="fa fa-twitter"></i></a></li>
                                 <li><a href="https://facebook.com/UWaveRadio" target = "_blank"><i class="fa fa-facebook"></i></a></li>
                                 <li><a href="https://github.com/UWave" target = "_blank"><i class="fa fa-github"></i></a></li>
-                                <li><a href="/listen.php" class="tunein topbutton"><i class="glyphicon glyphicon-play-circle playpause"></i></a></li>
                             </ul>
+                            <a href="/listen.php" class="tunein topbutton"><i class="glyphicon glyphicon-play-circle playpause"></i></a>
                         </span>
                     </div>
                 </div>
