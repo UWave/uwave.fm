5eb4e37c04c6427ae038d56b8e24efbfd874d914
Finn <thefinn93@thefinn93.com>
2014-05-09 12:54:33 -0700

    Get involved links should open in new tabs, except for internal links, which should be class navlink

--- getinvolved.php
+++ getinvolved.php
@@ -29,22 +29,22 @@
                     <h1>Get Involved</h1>
                     There are a number of ways to get involved in UWave:
                     <ul>
-                        <li><b><a href="https://docs.google.com/forms/d/18veHb-LUaAYtyhzKSUSoa0FAbl6jbYYRYrZ2QcehRS8/viewform">Host a show</a></b><br />
+                        <li><b><a href="https://docs.google.com/forms/d/18veHb-LUaAYtyhzKSUSoa0FAbl6jbYYRYrZ2QcehRS8/viewform" target="_blank">Host a show</a></b><br />
                             Have an idea for a music, talk or information show that you want to
                             bring to our community? Host a show at UWave Radio!<br />
                             No experience necessary, we will train you on how to use the equipment,
                             or help you with the details of pre-recording a show to play if you
                             aren't available to host your show from our broadcast studio. Fill out
                             the form at the link above to start the process. We suggest at least 1
-                            hour program length, and require that your show correspond with <a href="/about">UWave
+                            hour program length, and require that your show correspond with <a class="navlink" href="/about">UWave
                             Radio goals</a>.
                         <li><b>Attend a meeting</b><br />
                             UWave meets every week on Friday from 1:15pm to 3:15pm in HH1210 (on
-                            the <a href="https://www.uwb.edu">UW Bothell</a> campus). If you can't
+                            the <a href="https://www.uwb.edu" target="_blank">UW Bothell</a> campus). If you can't
                             make that, it's okay! Most departments meet at other times. If you know
                             which department you want to help out with, reach out to them by email
                             (<a href="contact">list of departments and their emails here</a>).
-                        <li><b><a href="https://docs.google.com/forms/d/1TyoM_hc1y2yPptSztskFrygPI4r6URx21GSx-1tbYNs/viewform">Submit an announcement</a></b><br />
+                        <li><b><a href="https://docs.google.com/forms/d/1TyoM_hc1y2yPptSztskFrygPI4r6URx21GSx-1tbYNs/viewform" target="_blank">Submit an announcement</a></b><br />
                             Interested in promoting your group or event through UWave? Fill out the
                             form above and smeone will get in contact with you.
                         </ul>
