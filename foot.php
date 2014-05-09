9e76d7cbc776a42a8553dd990aa387c62ec28313
Finn <thefinn93@thefinn93.com>
2014-05-08 22:17:01 -0700

    Load jq locally

--- foot.php
+++ foot.php
@@ -4,7 +4,7 @@
         </div>
     </div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="assets/js/bootstrap.min.js"></script>
     <script src="assets/js/uwave.js" type="text/javascript"></script>
