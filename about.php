8c5674c728090f8bf19e7975feca662f6ca4599f
Finn <thefinn93@thefinn93.com>
2014-05-14 03:02:50 -0700

    Added something to the about page since no one else would. 3am commits are always a good idea.

--- about.php
+++ about.php
@@ -26,17 +26,37 @@
                 <?php require('head.php'); ?>
                 <span class="pagecontents"><?php } ?>
                 <div class="content container">
-                    <h1>About UWave Radio</h1>
-                    UWave Radio is the campus-based community station of the Bothell area and
+                    <h1>About Us</h1>
+                    <p><i>UWave Radio is the campus-based community station of the Bothell area and
                     beyond. We are a center for community engagement, a hub for
                     underrepresented voices and music, an environment for professional
-                    development, and a catalyst for social justice.<br /><br />
+                    development, and a catalyst for social justice.</i></p><br />
 
-                    <b class="red">Broadcast Application Goal</b><br />
-                    We applied for the FCC's LPFM (Low Power FM)
-                    <a href="http://cdbs.recnet.net/fmq.php?facid=197326">broadcast application</a>
-                    in the end of 2013. If granted, will be broadcasting reaching a potential
-                    of 200,000 local listeners on 104.9FM!
+                    <p>We started in 2012, have been building a radio station from
+                      scratch at <a href="https://www.uwb.edu" target="_blank">UW Bothell</a>.
+                      We first started broadcasting online in the spring of 2013,
+                      filling the schedue as we can and the rest with automated playback.
+                      We applied for the FCC's LPFM (Low Power FM)
+                      <a href="http://cdbs.recnet.net/fmq.php?facid=197326">broadcast application</a>
+                      in the end of 2013. If granted, will be broadcasting an FM
+                      signal for the campus and beyond.</p>
+
+                    <p>Our organization is broken into four departments, who handle
+                      the key pieces of the station with the help of two directors
+                      who tie everything together and ensure that everything gets
+                      where it needs to be. The departments are as follows:</p>
+
+                      <ul>
+                        <li><b class="red">Business</b> handles everything businessy.
+                        Legal paperwork, funding sources, etc.</li>
+                        <li><b class="red">Programming</b> handles what's on the
+                          air. They organize shows, curate the music library.</li>
+                        <li><b class="red">Promotions</b> tells people about UWave.
+                          They coordinate our message, do outreach and work with
+                          other groups on and offcampus to hold events.</li>
+                        <li><b class="red">Technology</b> runs the computers, website
+                          and audio equipment, including the studio and servers.</li>
+                      </ul>
                 </div>
                 <?php if(!isset($_GET['contentonly'])) {?>
                 </span>
