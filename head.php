0274d0236b236eb8c678b9fe291e31a1c671d345
Finn <thefinn93@thefinn93.com>
2014-05-15 16:04:52 -0700

    Moved all of the <head> crap to the header file

--- head.php
+++ head.php
@@ -1,3 +1,28 @@
+<?php if(!isset($_GET['contentonly'])) { ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>UWave Radio</title>
+
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link href="assets/css/uwave.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="site-wrapper">
+        <div class="site-wrapper-inner">
+            <div class="cover-container">
                 <div class="masthead clearfix">
                     <div class="inner">
                         <span class="col-md-3">
@@ -30,3 +55,4 @@
                     <source src="http://live.uwave.fm:8000/listen-128.ogg" type="audio/ogg">
                     <source src="http://live.uwave.fm:8000/listen-128.mp3" type="audio/mp3">
                 </audio>
+                <span class="pagecontents"><?php } ?>
