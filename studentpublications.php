3e396843615621137127460bde57ea5394e8c1c1
Finn <thefinn93@thefinn93.com>
2014-05-16 12:40:15 -0700

    Mentioned that the other publications are totes our best bffs

--- studentpublications.php
+++ studentpublications.php
@@ -1,9 +1,9 @@
 <?php require('head.php'); ?>
                 <div class="content container">
-                    <h1>Other Student Publications</h1>
+                    <h1>Other Student Publications <small>(AKA our BFFs)</small></h1>
                     We are one of three stuent publications on campus:
                     <ul>
-                      <li><b><a href="http://www.huskyherald.com/" target="_blank" class="red">Husky Hearld</a></b>, the UW Bothell student newspaper.</li>
+                      <li><b><a href="http://www.huskyherald.com/" target="_blank" class="red">Husky Herald</a></b>, the UW Bothell student newspaper.</li>
                       <li><b><a href="http://uwbpolicyjournal.com/" target="_blank" class="red">UWB Policy Journal</a></b>, is a compilation of policy-related work submitted and published by students of the University of Washington Bothell.</li>
                     </ul>
                 </div>
