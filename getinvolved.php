fc7788eb7184dfa520cd70d2de5577de23ec07a5
Finn <thefinn93@thefinn93.com>
2014-05-16 14:01:24 -0700

    Headings are now bigger

--- getinvolved.php
+++ getinvolved.php
@@ -3,7 +3,7 @@
                     <h1>Get Involved</h1>
                     There are a number of ways to get involved in UWave:
                     <ul>
-                        <li><b class="red">Host a show</a></b><br />
+                        <li><h4 class="red">Host a show</h4>
                             Have an idea for a music, talk or information show that you want to
                             bring to our community? Host a show at UWave Radio!<br />
                             No experience necessary, we will train you on how to use the equipment,
@@ -12,13 +12,13 @@
                             <a href="https://docs.google.com/forms/d/18veHb-LUaAYtyhzKSUSoa0FAbl6jbYYRYrZ2QcehRS8/viewform" target="_blank">this form</a> at the link above to start the process. We suggest at least 1
                             hour program length, and require that your show correspond with <a class="navlink" href="/about">UWave
                             Radio goals</a>.
-                        <li><b class="red">Attend a meeting</b><br />
+                        <li><h4 class="red">Attend a meeting</h4>
                             UWave meets every week on Friday from 1:15pm to 3:15pm in HH1210 (on
                             the <a href="https://www.uwb.edu" target="_blank">UW Bothell</a> campus). If you can't
                             make that, it's okay! Most departments meet at other times. If you know
                             which department you want to help out with, reach out to them by email
                             (<a href="contact">list of departments and their emails here</a>).
-                        <li><b class="red">Submit an announcement</a></b><br />
+                        <li><h4 class="red">Submit an announcement</a></h4>
                             Interested in promoting your group or event through UWave? Fill out <a href="https://docs.google.com/forms/d/1TyoM_hc1y2yPptSztskFrygPI4r6URx21GSx-1tbYNs/viewform" target="_blank">this
                             form</a> above and smeone will get in contact with you.
                         </ul>
