d577dad1e5b91f8b71f59d446500e81aa360b2d7
Finn <thefinn93@thefinn93.com>
2014-05-16 13:59:27 -0700

    Clean up links on about page

--- getinvolved.php
+++ getinvolved.php
@@ -3,13 +3,13 @@
                     <h1>Get Involved</h1>
                     There are a number of ways to get involved in UWave:
                     <ul>
-                        <li><b><a href="https://docs.google.com/forms/d/18veHb-LUaAYtyhzKSUSoa0FAbl6jbYYRYrZ2QcehRS8/viewform" target="_blank" class="red">Host a show</a></b><br />
+                        <li><b class="red">Host a show</a></b><br />
                             Have an idea for a music, talk or information show that you want to
                             bring to our community? Host a show at UWave Radio!<br />
                             No experience necessary, we will train you on how to use the equipment,
                             or help you with the details of pre-recording a show to play if you
                             aren't available to host your show from our broadcast studio. Fill out
-                            the form at the link above to start the process. We suggest at least 1
+                            <a href="https://docs.google.com/forms/d/18veHb-LUaAYtyhzKSUSoa0FAbl6jbYYRYrZ2QcehRS8/viewform" target="_blank">this form</a> at the link above to start the process. We suggest at least 1
                             hour program length, and require that your show correspond with <a class="navlink" href="/about">UWave
                             Radio goals</a>.
                         <li><b class="red">Attend a meeting</b><br />
@@ -18,9 +18,9 @@
                             make that, it's okay! Most departments meet at other times. If you know
                             which department you want to help out with, reach out to them by email
                             (<a href="contact">list of departments and their emails here</a>).
-                        <li><b><a href="https://docs.google.com/forms/d/1TyoM_hc1y2yPptSztskFrygPI4r6URx21GSx-1tbYNs/viewform" target="_blank" class="red">Submit an announcement</a></b><br />
-                            Interested in promoting your group or event through UWave? Fill out the
-                            form above and smeone will get in contact with you.
+                        <li><b class="red">Submit an announcement</a></b><br />
+                            Interested in promoting your group or event through UWave? Fill out <a href="https://docs.google.com/forms/d/1TyoM_hc1y2yPptSztskFrygPI4r6URx21GSx-1tbYNs/viewform" target="_blank">this
+                            form</a> above and smeone will get in contact with you.
                         </ul>
                 </div>
                 <?php if(!isset($_GET['contentonly'])) {?>
