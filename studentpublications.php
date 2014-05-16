41595059dfeeee9bb32348be19f310292c3f5609
Finn <thefinn93@thefinn93.com>
2014-05-16 12:38:03 -0700

    Added a thing about other student publications

---
+++ studentpublications.php
@@ -0,0 +1,18 @@
+<?php require('head.php'); ?>
+                <div class="content container">
+                    <h1>Other Student Publications</h1>
+                    We are one of three stuent publications on campus:
+                    <ul>
+                      <li><b><a href="http://www.huskyherald.com/" target="_blank" class="red">Husky Hearld</a></b>, the UW Bothell student newspaper.</li>
+                      <li><b><a href="http://uwbpolicyjournal.com/" target="_blank" class="red">UWB Policy Journal</a></b>, is a compilation of policy-related work submitted and published by students of the University of Washington Bothell.</li>
+                    </ul>
+                </div>
+                <?php if(!isset($_GET['contentonly'])) {?>
+                </span>
+            </div>
+        </div>
+    </div>
+<?php require("foot.php"); ?>
+  </body>
+</html>
+<?php } ?>
