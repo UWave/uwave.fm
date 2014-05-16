12f2cc5d3a6f31f9a7a942ee8c7f801e853c8d76
Finn <thefinn93@thefinn93.com>
2014-05-16 01:19:57 -0700

    Created an embed page.

---
+++ embed.php
@@ -0,0 +1,37 @@
+<html>
+  <head>
+    <title>UWave Embed</title>
+    <base TARGET="_blank"> <!-- should make all links automatically open in a new tab/window -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/css/embed.css" rel="stylesheet">
+  </head>
+  <body>
+    <audio id="player" preload="none">
+        <source src="http://live.uwave.fm:8000/listen-128.ogg" type="audio/ogg">
+        <source src="http://live.uwave.fm:8000/listen-128.mp3" type="audio/mp3">
+    </audio>
+    <a href="/listen" class="btn btn-lg btn-play">
+      <img src="assets/img/swoosh.png" alt="UWave swoosh shape"><br />
+      <i class="playpause glyphicon glyphicon-play-circle"></i> <span class="playtext">Listen to UWave</span>
+    </a><br />
+    <span class="metadata"></span>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="assets/js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/uwave.js" type="text/javascript"></script>
+    <script>
+      $(document).ready(function() {
+          $(".btn-play").on("click", uwave.playpause);
+      });
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-40841528-1', 'uwave.fm');
+      ga('send', 'pageview');
+    </script>
+  </body>
+</html>
