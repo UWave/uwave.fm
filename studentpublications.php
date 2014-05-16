4f454ea380fc7cf8ed69d56406960315a06bb741
Finn <thefinn93@thefinn93.com>
2014-05-16 12:58:43 -0700

    Oh yeah clamor is cool

--- studentpublications.php
+++ studentpublications.php
@@ -1,10 +1,11 @@
 <?php require('head.php'); ?>
                 <div class="content container">
                     <h1>Other Student Publications <small>(AKA our BFFs)</small></h1>
-                    We are one of three stuent publications on campus:
+                    We are one of four stuent publications on campus:
                     <ul>
                       <li><b><a href="http://www.huskyherald.com/" target="_blank" class="red">Husky Herald</a></b>, the UW Bothell student newspaper.</li>
                       <li><b><a href="http://uwbpolicyjournal.com/" target="_blank" class="red">UWB Policy Journal</a></b>, is a compilation of policy-related work submitted and published by students of the University of Washington Bothell.</li>
+                      <li><b><a href="http://clamor-journal.com/" target="_blank" class="red">Clamor</a></b>, is the literary and arts journal at UWB.</li>
                     </ul>
                 </div>
                 <?php if(!isset($_GET['contentonly'])) {?>
