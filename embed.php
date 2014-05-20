caa660c96576c43eb1cace70a68104a9035a4ca2
Finn <thefinn93@thefinn93.com>
2014-05-20 02:23:46 -0700

    Enhanced Link Attribution

--- embed.php
+++ embed.php
@@ -31,6 +31,7 @@
       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
       ga('create', 'UA-40841528-1', 'uwave.fm');
+      ga('require', 'linkid', 'linkid.js');
       ga('require', 'displayfeatures');
       ga('send', 'pageview');
     </script>
