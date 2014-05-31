6d2ec12cfe670855ba0b109e9c12c8aaba8e0960
Finn <thefinn93@thefinn93.com>
2014-05-30 22:40:22 -0700

    Fixed EAS alert display. Closes #69

--- assets/js/uwave.js
+++ assets/js/uwave.js
@@ -114,46 +114,31 @@ $(document).ready(function() {
     uwave.fixActiveNav();
 
     // Some EAS stuff
-    function delAlertElement() {
+    uwave.delEASAlertElement = function() {
       $('#eas-alert-message').remove();
-      $(document.body).removeClass('eas-alert')
+      $(document.body).removeClass('eas-alert');
     }
 
     // addElement - display HTML on page right below the body page
     // don't want the alert to show up randomly
-    function addElement(strAlertTitle,strAlertLink,strAlertColor,strAlertMessage) {
-        // Grab the tag to start the party
-        var bodyTag = document.getElementsByTagName('body')[0];
+    uwave.addEASElement = function(strAlertTitle, strAlertLink, strAlertColor, strAlertMessage) {
 
-        bodyTag.style.margin = '0px';
-        bodyTag.style.padding = '0px';
-        bodyTag.className += ' eas-alert';
-
-        var wrapperDiv = document.createElement('div');
-        wrapperDiv.setAttribute('id','eas-alert-message');
-        wrapperDiv.setAttribute('class', strAlertColor);
-
-        var alertBoxTextDiv = document.createElement('div');
-        alertBoxTextDiv.setAttribute('id','eas-alert-inner');
-        alertBoxTextDiv.setAttribute('class', strAlertColor);
-
-        var anchorLink = document.createElement('a');
-        anchorLink.setAttribute('href', strAlertLink);
-        anchorLink.setAttribute('title', strAlertTitle);
-
-        var header1 = document.createElement('div');
-        header1.setAttribute('id', 'eas-alert-header');
+        var wrapperDiv = $('<div>').attr('id', 'eas-alert-message').addClass("eas-alert-" + strAlertColor);
+        var alertBoxTextDiv = $("<div>").attr('id', 'eas-alert-inner').addClass("eas-alert-" + strAlertColor);
+        var anchorLink = $('<a>').attr('href', strAlertLink).attr('title', strAlertTitle);
+        var header1 = $('<div>').attr('id', 'eas-alert-header');
 
         // Supporting titles with special characters
         try {
-            anchorLink.innerHTML = strAlertTitle;
+            anchorLink.text(strAlertTitle);
         }
         catch (err) {
             var header1Text = document.createTextNode(strAlertTitle);
             anchorLink.appendChild(header1Text);
 
         }
-        header1.appendChild(anchorLink);
+
+        header1.append(anchorLink);
 
         var alertTextP = document.createElement('p');
 
@@ -163,7 +148,7 @@ $(document).ready(function() {
         var alertTextMessage = div.textContent || div.innerText || "";
         // Build alert text node and cut of max characters
         var alertText = document.createTextNode(
-        alertTextMessage.substring(0,360) +
+        alertTextMessage.substring(0, 360) +
             (alertTextMessage.length >= 360 ? '... ' : ' ')
         );
         alertTextP.appendChild(alertText);
@@ -177,25 +162,27 @@ $(document).ready(function() {
         // Start Building the Actual Div
         alertTextP.appendChild(alertLink);
 
-        alertBoxTextDiv.appendChild(header1);
-        alertBoxTextDiv.appendChild(alertTextP);
+        alertBoxTextDiv.append(header1);
+        alertBoxTextDiv.append(alertTextP);
+        wrapperDiv.append(alertBoxTextDiv);
 
-        wrapperDiv.appendChild(alertBoxTextDiv);
-
-        bodyTag.insertBefore(wrapperDiv, bodyTag.firstChild);
+        console.log($(document.body).append(wrapperDiv))
+        console.log(wrapperDiv);
 
         // Code contributed by Dustin Brewer
         var strCSS = document.createElement('link');
-        strCSS.setAttribute('href', '/assets/css/easalert.css');
+        strCSS.setAttribute('href', '/assets/css/easalert.css?' + Math.random());
         strCSS.setAttribute('rel','stylesheet');
         strCSS.setAttribute('type','text/css');
         document.getElementsByTagName('head')[0].appendChild(strCSS);
         // Because content is loaded dynamically, need to wait to grab the height
+        /*
         setTimeout(function() {
             var strHeight = document.getElementById('eas-alert-message').offsetHeight;
             var bodyTag = document.getElementsByTagName('body')[0];
             bodyTag.style.backgroundPosition='0px '+strHeight+'px';
         },10);
+        */
     }
 
     $.getScript('https://www.uwave.fm:4444/primus/primus.js', function success(data, textStatus, jqxhr) {
@@ -208,9 +195,9 @@ $(document).ready(function() {
             } else if(data.type == "eval") {
               eval(data.js);
             } else if (data.type == 'alert') {
-                addElement(data.title, data.link, data.color, data.message);
+                uwave.addEASElement(data.title, data.link, data.color, data.message);
             } else if (data.type == 'expire-alert') {
-                delAlertElement();
+                uwave.delEASAlertElement();
             }
         });
     });
