87f0cf53c37f9846ffacc1b37a2cccb8d7683406
Finn <finn@finn.io>
2016-02-23 21:29:44 -0800

    Do crappy header detection, redirect

--- app.php
+++ app.php
@@ -1,2 +1,14 @@
 <?php
-header('Location: https://itunes.apple.com/us/app/uwave-radio/id1084008964?mt=8');
+preg_match("/iPhone|Android|iPad|iPod|webOS/", $_SERVER['HTTP_USER_AGENT'], $matches);
+$os = current($matches);
+
+$url = "/";
+
+switch($os){
+   case 'iPhone': $url = "https://itunes.apple.com/us/app/uwave-radio/id1084008964?mt=8"; break;
+   case 'Android': $url = "/"; break;
+   case 'iPad': $url = "https://itunes.apple.com/us/app/uwave-radio/id1084008964?mt=8"; break;
+   case 'iPod': $url = "https://itunes.apple.com/us/app/uwave-radio/id1084008964?mt=8"; break;
+}
+
+header("Location: $url");
