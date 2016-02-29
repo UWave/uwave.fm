5dd00c5a32d253e20c7a4cf573d389ac82dcb9ee
donald <donald@uwave.fm>
2016-02-29 13:39:58 -0800

    add about template

--- uwave.py
+++ uwave.py
@@ -9,7 +9,7 @@ def index():
 
 @app.route("/about")
 def about():
-    return render_template('index.html')
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
