92175e4517995f16ae4dba1d338a1404909a56ff
Finn <finn@finn.io>
2016-02-29 13:46:07 -0800

    Dynamically pick the page

--- uwave.py
+++ uwave.py
@@ -1,15 +1,19 @@
 #! /usr/bin/env python
 
 from flask import Flask, render_template
+import jinja2.exceptions
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-@app.route("/about")
-def about():
-    return render_template('about.html')
+@app.route("/<page>")
+def about(page):
+    try:
+        return render_template('%s.html' % page)
+    except jinja2.exceptions.TemplateNotFound:
+        return "404"
 
 if __name__ == "__main__":
     app.run(debug=True)
