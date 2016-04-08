7d3bf84463c0cc9fde8323c29dd26b5bc8193414
Finn <finn@uwave.fm>
2016-04-08 13:51:31 -0700

    Move events.php to at-your-event.php, make a mock-up of an events page

--- events.php
+++ events.php
@@ -1,16 +1,13 @@
 <?php require('head.php'); ?>
                 <div class="content container">
-                    <h1>UWave at Your Event!</h1>
-                    UWave will come to your on campus event and play music. To make this happen, we need
-                    a few things
-                    <ul>
-                      <li /> <b>At least 1 month notice</b>
-                      <li /> A table for our equipment. Generally 2x4' or larger
-                      <li /> If the event is outside, a tent
-                      <li /> Access to power (eg a spider box for outdoor events)
-                      <li /> For on campus events, a cart to move our equipment
-                    </ul>
-                    Once you've ensured that you have those things fill out <a href="https://docs.google.com/forms/d/1STT_SlNuJ9-Bm5B47V8CK-iBE29GnuFQaD0X00CzjuU/viewform">this form</a>
+                    <h1>UWave Events</h1>
+                    <h2>The Awesome Diversity Dance Party Thing</h2>
+                    <b>April 10, 5:30pm-8:30pm, <a href="#">UW2 943</a></b><br />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit, mauris ac maximus varius, eros metus commodo felis, in laoreet nisl dui sed quam. Phasellus aliquet orci finibus scelerisque faucibus. Mauris ac nisl vel ligula finibus vulputate. Integer semper scelerisque ante id sagittis. Nunc malesuada efficitur dui quis euismod. Etiam at volutpat magna, eget suscipit eros. Pellentesque consequat turpis non rutrum lobortis. Mauris at consequat neque, non mattis ligula. Donec pellentesque lectus vel sem interdum scelerisque. Vivamus cursus neque a eros accumsan molestie. Phasellus sit amet imperdiet mauris. Fusce ut tempor lacus. Suspendisse facilisis, mauris nec lobortis venenatis, dui eros bibendum dui, eget vulputate erat enim quis turpis.</p>
+
+                    <h2>Another fun event</h2>
+                    <b>April 11, 7:45pm-9:00pm, <a href="#">UW4 999</a></b>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit, mauris ac maximus varius, eros metus commodo felis, in laoreet nisl dui sed quam. Phasellus aliquet orci finibus scelerisque faucibus. Mauris ac nisl vel ligula finibus vulputate. Integer semper scelerisque ante id sagittis. Nunc malesuada efficitur dui quis euismod. Etiam at volutpat magna, eget suscipit eros. Pellentesque consequat turpis non rutrum lobortis. Mauris at consequat neque, non mattis ligula. Donec pellentesque lectus vel sem interdum scelerisque. Vivamus cursus neque a eros accumsan molestie. Phasellus sit amet imperdiet mauris. Fusce ut tempor lacus. Suspendisse facilisis, mauris nec lobortis venenatis, dui eros bibendum dui, eget vulputate erat enim quis turpis.</p>
                 </div>
                 <?php if(!isset($_GET['contentonly'])) {?>
                 </span>
