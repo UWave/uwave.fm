f175e6944a261678b94271caddf0e55da54ee958
Finn <finn@uwave.fm>
2016-05-13 21:57:25 -0700

    end properly
    also fix another important thing

--- slack/remotebroadcast.php
+++ slack/remotebroadcast.php
@@ -2,15 +2,21 @@
 $arg = $_POST['text'];
 $message = "Something's broken pretty bad. Sorry ¯\_(ツ)_/¯";
 
+function affirmative_response() {
+  $response = array("k", "mkay", "whatever", "done", "uggg fine");
+  return response[array_random($response)];
+}
+
 switch($arg) {
   case "remotebroadcast start":
     exec("rmlsend 'PX 1 050035!'");  # Record Game Night
     exec("rmlsend 'PX 1 050002!'");  # Trigger remote broadcast
-    $message = "k";
+    $message = affirmative_response();
   break;
   case "remotebroadcast stop":
-    exec("rmlsend 'PX 1 050004!'");
-    $message = "k";
+    exec("rmlsend 'PX 1 050004!'");  # add the End Remote Broadcast cart to next
+    exec("rmlsend 'PN 1!'");  # Play the next cart
+    $message = affirmative_response();
   break;
   default:
     $message = "Yeah i dunno what to do about that.";
