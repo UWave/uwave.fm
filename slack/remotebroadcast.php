6fe7ddbedf1e2036b2a071f4b9686be6c6c59e13
Finn <finn@uwave.fm>
2016-05-13 21:15:39 -0700

    Add remotebroadcast hook

---
+++ slack/remotebroadcast.php
@@ -0,0 +1,20 @@
+<?php
+$arg = $_POST['text'];
+$message = "Something's broken pretty bad. Sorry ¯\_(ツ)_/¯";
+switch($arg) {
+  case "start":
+    $message = exec("rmlsend PX 0 050002!");
+  break;
+  case "stop":
+    $message = exec("rmlsend PX 0 050004!");
+  break;
+  default:
+    $message = "Yeah i dunno what to do about that.";
+  break;
+}
+
+echo json_encode(array(
+  "text" => $message,
+  "username" => "Remote Broadcast Controller",
+  "icon_url" => "https://uwave.fm/assets/img/uwave-square.png"
+));
