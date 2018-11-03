7c15c526e8d86d30b9042fad1d9e39360f461001
bencov <bencov14@gmail.com>
2018-11-02 18:36:11 -0700

    testing

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php require('head.php'); ?>
                 <div class="jumbotron red">
                     <h1>404 <small>Page not found</small></h1>
-                    <p>Sorry, the page you requested could not be found</p>
+                    <p>Whoops! Looks like the page you requested is not here</p>
                 </div>
                 <?php if(!isset($_GET['contentonly'])) {?>
                 </span>
